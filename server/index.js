e0e50256119b9b6ee08ce9bc8bf3fc0cb587cb0d
Prathmesh895 <125139465+Prathmesh895@users.noreply.github.com>
2024-08-10 21:53:30 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -1,53 +1,10 @@
-// const express = require('express');
-// const { ConnectDB } = require('./lib/conectDB');
-// const app = express();
-// require('dotenv').config();
-// const path = require('path')
-// const cors = require('cors');
-// // const PORT = 5000;
-// const bookRoute = require('./router/books')
-// const loginRoute = require('./router/login');
-// const signupRoute = require('./router/signup')
-// const forpassRoute =  require('./router/password');
-// const cookieParser = require('cookie-parser')
-
-
-// app.use('/files', express.static(path.join(__dirname, 'files')));
-
-// app.use(express.json());
-// app.use(cors({
-//     origin : ["https://book-store-prathmeshgl.vercel.app"],
-//     credentials:true
-// }));
-
-// app.use(cookieParser())
-
-// app.use('/books', bookRoute);
-// app.use('/api', loginRoute);
-// app.use('/passWord', forpassRoute);
-// app.use('/api', signupRoute);
-
-// app.get(('/'), (req, res) => {
-//     console.log(req);
-//     return res.status(200).send("Welcome");
-// })
-
-
-// ConnectDB();
-
-// // app.listen(PORT, () => {
-// //     console.log(`server is listening at ${PORT}`)
-// // })
-
-// module.exports = app;
-
-
 const express = require('express');
 const { ConnectDB } = require('./lib/conectDB');
 const app = express();
 require('dotenv').config();
 const path = require('path');
 const cors = require('cors');
+// const PORT = 5000;
 const bookRoute = require('./router/books');
 const loginRoute = require('./router/login');
 const signupRoute = require('./router/signup');
@@ -56,10 +13,10 @@ const cookieParser = require('cookie-parser');
 
 // CORS configuration
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "https://book-store-prathmeshgl.vercel.app",
     credentials: true,
 }));
-app.options('*', cors()); 
+app.options('*', cors()); // Handle preflight requests
 
 // Serve static files
 app.use('/files', express.static(path.join(__dirname, 'files')));
@@ -87,6 +44,10 @@ app.get('/', (req, res) => {
 
 // Connect to the database
 ConnectDB();
+
+// app.listen(PORT, () => {
+//     console.log(`server is listening at ${PORT}`)
+// })
 
 // Module export for serverless deployment
 module.exports = app;
