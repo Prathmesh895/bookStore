7477d4022dc0cdaafdcf6722678946a98b498942
Prathmesh895 <125139465+Prathmesh895@users.noreply.github.com>
2024-08-11 20:55:11 +0530

    first commit

--- server/router/books.js
+++ server/router/books.js
@@ -6,7 +6,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../files');
+    cb(null, '/files');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
